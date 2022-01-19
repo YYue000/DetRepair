d4ed52d07cffd76d5d53a53efed26774d84eafa6
yueyuxin <yyue1@ualberta.ca>
2022-01-19 10:22:54 +0900

    modify eval_one_img

--- eval_one_img.py
+++ eval_one_img.py
@@ -10,9 +10,16 @@ cocoGt = COCO(anno_file)
 
 metric='bbox'
 predictions = json.load(open(box_file))
+cocoDt = cocoGt.loadRes(predictions)
+cocoEval = COCOeval(cocoGt, cocoDt, iouType=metric)
+cocoEval.evaluate()
+cocoEval.accumulate()
+cocoEval.summarize()
+
+
 imgids = cocoGt.getImgIds().copy()
 
-ev_res = np.zeros([6,len(imgids)])
+ev_res = np.zeros([12,len(imgids)])
 for i,imgid in enumerate(imgids):
     pred = [_ for _ in predictions if _['image_id']==imgid]
     cocoDt = cocoGt.loadRes(pred)
@@ -22,7 +29,7 @@ for i,imgid in enumerate(imgids):
     cocoEval.accumulate()
     cocoEval.summarize()
     ap = cocoEval.stats
-    print(imgid, ap)
+    print('Evaluating',imgid, ap)
     ev_res[:,i] = ap
 
 import pickle
